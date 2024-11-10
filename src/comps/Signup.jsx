46943e2ac27488d6294c18d076b84e8cedb09f72
Jason Ie <ie.jasonray10@gmail.com>
2024-11-10 03:04:41 -0500

    user title fix

--- src/comps/Signup.jsx
+++ src/comps/Signup.jsx
@@ -93,7 +93,7 @@ export default function Signup() {
 								type="text"
 								required
 								className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-								placeholder="Your Name Here"
+								placeholder="What should we call you?"
 								value={name}
 								onChange={(e) => setName(e.target.value)}
 							/>
